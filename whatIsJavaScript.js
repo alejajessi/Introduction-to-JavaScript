7232a1b8723abf8b2992591c735083f1f5e21a9f
alejajessi <alejandra122098@gmail.com>
2020-02-15 20:28:35 -0500

    Console with my programming weeks

---
+++ whatIsJavaScript.js
@@ -0,0 +1,14 @@
+
+// En este repositorio, obtendré fundamentos de JavaScript
+
+/*Console -  imprime lo que está en los parentesis. 
+Se imprimirá el número 5.
+*/
+
+console.log(5);
+
+/* Está segunda línea es con la cantidad
+ de semanas que he programado
+*/
+
+console.log(500);
