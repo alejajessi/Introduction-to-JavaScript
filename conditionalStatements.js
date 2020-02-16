95e7a658d44db185e042cbec16af31752b315243
alejajessi <alejandra122098@gmail.com>
2020-02-15 22:28:20 -0500

    No more for today

---
+++ conditionalStatements.js
@@ -0,0 +1,31 @@
+//Lección Conditional Statements
+
+//Inicio de ejercicio The if keyword
+//Punto 1. Declare una variable let llamada sale e iguale a true.
+//Punto 2. Haga uso del if, coloque sale como parametro y dentro imprima 'Time to buy!'
+//Punto 3. Cambie el valor de sale a false, antes de iniciar el if.
+
+let sale=true;
+ sale=false;
+if(sale){
+  console.log('Time to buy!');
+}
+
+//Inicio de ejercicio if...else statements
+//Punto 1. Teniendo en cuenta el código inicial, añada un else
+//         e imprima 'Time to wait for a sale.'
+
+ /*Código copiado de Codecademy
+ Curso: Introduction to JavaScript
+ */
+
+let sale = true;
+
+sale = false;
+
+if(sale) {
+  console.log('Time to buy!');
+} else{
+  console.log('Time to wait for a sale.');
+}
+
