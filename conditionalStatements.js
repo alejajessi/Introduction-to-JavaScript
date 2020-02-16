ef26c07d7193634e44c14d66541f86e42ca763e0
alejajessi <alejandra122098@gmail.com>
2020-02-16 10:24:20 -0500

    Lesson about conditionals

--- conditionalStatements.js
+++ conditionalStatements.js
@@ -29,3 +29,59 @@ if(sale) {
   console.log('Time to wait for a sale.');
 }
 
+//Inicio de ejercicio Comparison Operators
+//Punto 1. Declare una variable let llamada hungerLever e iguale a 7.
+//Punto 2. Cree un if...else. La condición es que la variable hungerLevel sea mayor a 7.
+//         Dentro del if imprima 'Time to eat!' y del else imprima 'We can eat later!')
+
+let hungerLevel=7;
+if (hungerLevel > 7){
+  console.log('Time to eat!');
+} else {
+  console.log('We can eat later!');
+}
+
+//Inicio de ejercicio Logical Operators
+//Punto 1. Cree en un if...else. El condicional es:
+//         mood debe ser 'sleepy' y tirednessLevel debe ser mayor a 8
+//         Dentro del if, imprima 'time to sleep' y en el else, imprimir
+//         'not bed time yet'
+
+ /*Código copiado de Codecademy
+ Curso: Introduction to JavaScript
+ */
+
+let mood = 'sleepy';
+let tirednessLevel = 6;
+
+if (mood === 'sleepy' && tirednessLevel > 8){
+  console.log('time to sleep');
+} else{
+  console.log('not bed time yet');
+}
+
+//Inicio de ejercicio Truthy and Falsy
+//Punto 1. Cambie el valor de la variable wordCount para que entre al if.
+//Punto 2. Cambie el valor de la variable favoritePhrase para que entre al else.
+
+/*Código copiado de Codecademy
+ Curso: Introduction to JavaScript
+ */
+
+let wordCount = 1;
+
+if (wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+
+
+let favoritePhrase = '';
+
+if (favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
+}
+
