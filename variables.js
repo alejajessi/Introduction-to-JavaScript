332395584e64d6ef263006caef00c02a67cd0d53
alejajessi <alejandra122098@gmail.com>
2020-02-15 21:37:33 -0500

    Finish of first lesson

---
+++ variables.js
@@ -0,0 +1,19 @@
+//Inicio lección 2 -Variables
+
+//Inicio de ejercicio create a variable:var
+//Punto 1. Declare una variable llamada favoriteFood e iguale a 'pizza'
+//Punto 2. Declare una variable llamada numOfSlices e iguale a 8.
+//Punto 3. Imprima el valor guardado en los dos puntos anteriores.
+
+var favoriteFood='pizza';
+var numOfSlices=8;
+console.log(favoriteFood);
+console.log(numOfSlices);
+
+//Inicio de ejercicio create a variable:let
+//Punto 1. Declare una variable llamada changeMe e iguale a true
+//Punto 2. Haciendo uso de la variable declarada, cambie su valor a false e imprimalo.
+
+let changeMe=false
+console.log(changeMe);
+
