3ddb88fd71452ffd0bc88b800961ddc8ab747d87
alejajessi <alejandra122098@gmail.com>
2020-02-15 20:37:44 -0500

    More exercises about introduction

--- whatIsJavaScript.js
+++ whatIsJavaScript.js
@@ -12,3 +12,33 @@ console.log(5);
 */
 
 console.log(500);
+
+/*Código copiado de CodeAcademy
+Curso: Introduction to JavaScript
+*/
+
+/*Este es un multi-line comment se denota con "/*"
+*/
+
+//Inicio de ejercicio comments.
+//Punto 1. comentar "Opening line"
+//Punto 2. Haciendo uso de multi-line, comente desde la línea 29 a 33. 
+
+//Opening line
+console.log('The first time Yossarian saw the chaplain he fell madly in love with him.');
+/*
+console.log('Yossarian was in the hospital with a pain in his liver that fell just short of being jaundice.');
+console.log('The doctors were puzzled by the fact that it wasn\'t quite jaundice.');
+console.log('If it became jaundice they could treat it.');
+console.log('If it didn\'t become jaundice and went away they could discharge him.');
+console.log('But this just being short of jaundice all the time confused them.');
+*/
+
+//Inicio de ejercicio Data types
+//Punto 1. Haciendo uso del log y el tipo de dato String comente 'JavaScript'
+//Punto 2. Haciendo uso del log y el tipo de dato numero comente 2011
+
+console.log('JavaScript');
+console.log(2011);
+
+
