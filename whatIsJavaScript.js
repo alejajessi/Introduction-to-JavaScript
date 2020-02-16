fcbf3594c952fcc28837b6e2b2e7eb3da2108f35
alejajessi <alejandra122098@gmail.com>
2020-02-15 20:50:08 -0500

    More exercises about introduction x2

--- whatIsJavaScript.js
+++ whatIsJavaScript.js
@@ -37,8 +37,33 @@ console.log('But this just being short of jaundice all the time confused them.')
 //Inicio de ejercicio Data types
 //Punto 1. Haciendo uso del log y el tipo de dato String comente 'JavaScript'
 //Punto 2. Haciendo uso del log y el tipo de dato numero comente 2011
+//Punto 3. Imprimir 'Woohoo! I love to code! #codecademy'
+//Punto 4. Imprimir el número "20.49"
 
 console.log('JavaScript');
 console.log(2011);
+console.log('Woohoo! I love to code! #codecademy');
+console.log(20.49);
 
+//Inicio de ejercicio Arithmetic Operators
+//Punto 1. Sumar 3.5 a mi edad e imprimir.
+//Punto 2. Restar 1969 al año presente (2020) e imprmir
+//Punto 3. Dividir 65 por 240 e imprimir
+//Punto 4. Multiplicar 0.2708 por 100 e imprimir
 
+console.log(21+3.5);
+console.log(2020-1969);
+console.log(65/240);
+console.log(0.2708*100);
+
+//Inicio de ejercicio String concatenation
+//Punto 1. Concatenar dos String 'Hello' 'World'
+//Punto 2. Concatenar a la oración anterior un espacio  en medio de las dos palabras ' '
+
+console.log('Hello' + 'World');
+console.log('Hello' + ' ' + 'World');
+
+//Inicio de ejercicio properties
+//Punto 1. Generar el número de caracteres de la oración 'Teaching the world how to code'
+
+console.log('Teaching the world how to code'.length);
