564bde5ac1dff9ba06474d20563804114acd2b04
alejajessi <alejandra122098@gmail.com>
2020-02-16 22:39:52 -0500

    No more for today. I am in loops lesson

---
+++ loops.js
@@ -0,0 +1,13 @@
+//Inicio lección  loops
+
+//Inicio de ejercicio repeating tasks manually
+//Punto 1. Crear variable vacationSpots y añada
+//         tres ciudades que quiera visitar.
+//Punto 2. Imprima uno a uno los elementos del array.
+
+// Write your code below
+const vacationSpots =['Aruba','Curacao','New York'];
+
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
