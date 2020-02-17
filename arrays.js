0379b41461a8ad150c3382df1db911d11240cbd4
alejajessi <alejandra122098@gmail.com>
2020-02-16 20:14:34 -0500

    No more for this moment - I am in section 5

---
+++ arrays.js
@@ -0,0 +1,10 @@
+//Inicio de lección Arrays
+
+//Inicio de ejercicio create an Array
+//Punto 1. Declare un array con 3 strings conformandolo.
+//Punto 2. Imprima hobbies
+
+const hobbies = ['Keep a journal', 'Take a falconry class', 'Learn to juggle'];
+
+console.log(hobbies);
+
