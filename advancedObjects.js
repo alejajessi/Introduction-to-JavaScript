747a949bfed0fec6caf0e1084ee131eac1ece9d0
alejajessi <alejandra122098@gmail.com>
2020-02-17 20:23:59 -0500

    Finis lesson advanced objects

--- advancedObjects.js
+++ advancedObjects.js
@@ -74,3 +74,79 @@ const robot = {
     } 
 };
 
+//Inicio de ejercicio factory functions
+
+const robotFactory = (model, mobile) => {
+    return {
+      model : model,
+          mobile: mobile,
+          beep () { 
+        console.log('Beep Boop'); 
+      }
+      };
+};
+  
+const tinCan = robotFactory('P-500', true);
+tinCan.beep();
+
+//Inicio de ejercicio property value shorthand
+
+function robotFactory(model, mobile){
+    return {
+      model,
+      mobile,
+      beep() {
+        console.log('Beep Boop');
+      }
+    }
+}
+  
+  
+// To check that the property value shorthand technique worked:
+const newRobot = robotFactory('P-501', false)
+console.log(newRobot.model)
+console.log(newRobot.mobile)
+
+//Inicio de ejercicio destructured assignment
+
+const robot = {
+    model: '1E78V2',
+    energyLevel: 100,
+    functionality: {
+      beep() {
+        console.log('Beep Boop');
+      },
+      fireLaser() {
+        console.log('Pew Pew');
+      },
+    }
+};
+  
+const { functionality } = robot;
+  
+functionality.beep();
+
+//Inicio de ejercicio built-in object methods
+
+const robot = {
+	model: 'SAL-1000',
+  mobile: true,
+  sentient: false,
+  armor: 'Steel-plated',
+  energyLevel: 75
+};
+
+// What is missing in the following method call?
+const robotKeys = Object.keys(robot);
+
+console.log(robotKeys);
+
+// Declare robotEntries below this line:
+const robotEntries = Object.entries(robot)
+console.log(robotEntries);
+
+// Declare newRobot below this line:
+const newRobot = Object.assign({laserBlaster: true, voiceRecognition: true}, robot);
+
+console.log(newRobot);
+
